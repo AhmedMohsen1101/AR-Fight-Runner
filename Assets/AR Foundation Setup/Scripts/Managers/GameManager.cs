a102a32004d10df8317cfee47e54c279e5f3b829
AhmedMohsen1101 <50053704+AhmedMohsen1101@users.noreply.github.com>
2021-08-01 06:39:25 +0200

    Initial part two

---
+++ Assets/AR Foundation Setup/Scripts/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FrameRate
+{
+    FPS_30 = 30,
+    FPS_60 = 60,
+    FPS_70 = 70,
+    FPS_90 = 90,
+    FPS_120 = 120,
+    FPS_144 = 144,
+
+}
+public class GameManager : MonoBehaviour
+{
+    public FrameRate frameRate;
+    private void Awake()
+    {
+        Application.targetFrameRate = (int)frameRate;
+
+
+        DontDestroyOnLoad(this);
+    }
+   
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }   
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
